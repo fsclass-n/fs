a6a61f69b8d72a25cf3c8165ed7b7cd720e52833
fsclass-n <whyeil@naver.com>
2026-04-16 13:57:38 +0900

    입출력

--- py_basic/ch06_입출력/ex05_with문.py
+++ py_basic/ch06_입출력/ex05_with문.py
@@ -1,9 +1,9 @@
 target_path = "D:\\wi\git\\fs\\py_basic\\ch06_입출력"
 
 # r(Raw String): 날 머신 문자열
-base_path = r"D:\wi\git\fs"
+base_path = r"D:\wi\git"
 
-relative_path = "py_basic/ch06_입출력"
+relative_path = "fs/py_basic/ch06_입출력"
 full_path = base_path + "/" + relative_path
 print(full_path)
 
