f5939b0886a7e6e923aa2fb2b321ef7edc914a42
fsclass-n <whyeil@naver.com>
2026-04-29 09:25:03 +0900

    11

--- rpa/p01_웹/ch01_requests/ex05-2_txt파일저장.py
+++ rpa/p01_웹/ch01_requests/ex05-2_txt파일저장.py
@@ -7,13 +7,21 @@ target_url = "https://www.gutenberg.org/files/57362/57362-0.txt"
 try:
     response = requests.get(target_url)
     response.raise_for_status()
+    print("요청한 응답 정보는?", response) # <Response [200]>
+    print("요청한 응답 코드는?", response.status_code) # 200
+    print("요청한 응답 헤더는?", response.headers)
+    # print("요청한 응답 텍스트는?", response.text)
 
     # 현재 실행 중인 .py 파일의 절대 경로를 가져옵니다.
     current_file_path = os.path.abspath(__file__)
     print(current_file_path)
     # 그 파일이 들어있는 '폴더' 경로만 추출합니다.
-    current_dir = os.path.dirname(current_file_path)
+    current_dir = os.path.join(os.path.dirname(current_file_path), "data")
     print(current_dir)
+
+    # 만약 해당 폴더가 없다면 자동으로 생성해주는 코드 (안전장치)
+    if not os.path.exists(current_dir):
+        os.makedirs(current_dir)
 
     # 폴더 경로와 파일 이름을 합쳐줍니다.
     save_path = os.path.join(current_dir, "윤동주_시.txt")
@@ -34,16 +42,13 @@ except requests.exceptions.HTTPError as e:
 else:
     print("페이지 로딩 성공:", response.status_code)
 
-
 '''
-# current_file_path = os.path.abspath(__file__)
-d:\wi\git\fs\rpa\p01_웹\ch01_requests\ex05-1_txt파일저장.py
-
-# current_dir = os.path.dirname(current_file_path)
-d:\wi\git\fs\rpa\p01_웹\ch01_requests
-
-# save_path = os.path.join(current_dir, "윤동주_시.txt")
-d:\wi\git\fs\rpa\p01_웹\ch01_requests\윤동주_시.txt
+요청한 응답 정보는? <Response [200]>
+요청한 응답 코드는? 200
+요청한 응답 헤더는? {'date': 'Tue, 28 Apr 2026 06:22:22 GMT', 'server': 'Apache', 'last-modified': 'Wed, 20 Jun 2018 12:45:24 GMT', 'accept-ranges': 'bytes', 'content-length': '396000', 'x-backend': 'gutenweb6', 'content-type': 'text/plain; charset=utf-8'}
+d:\wi\git\fs\rpa\p01_웹\ch01_requests\ex05-2_txt파일저장.py
+d:\wi\git\fs\rpa\p01_웹\ch01_requests\data
+d:\wi\git\fs\rpa\p01_웹\ch01_requests\data\윤동주_시.txt
 1
 2
 3
