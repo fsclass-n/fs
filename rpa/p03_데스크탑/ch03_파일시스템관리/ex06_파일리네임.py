9473ccd3ca705caaf97d2ca36ba1df4789943c75
fsclass-n <whyeil@naver.com>
2026-05-14 16:57:48 +0900

    11

--- rpa/p03_데스크탑/ch03_파일시스템관리/ex06_파일리네임.py
+++ rpa/p03_데스크탑/ch03_파일시스템관리/ex06_파일리네임.py
@@ -5,28 +5,28 @@ from pathlib import Path
 # 매출보고서_2026_01_01.xlsx
 
 DATE_KR = re.compile(r'(.*?)(\d{4})년(\d{2})월(\d{2})일(.*)')
-'''
-r''	    Raw 문자열 (\를 그대로 사용)
 
-기호	설명
-( )	    그룹(캡처 그룹)
-.	    아무 문자 1개
-*	    0개 이상 반복
-+       1개 이상 반복
-?	    최소 매칭(Non-Greedy)
-.*	    모든 문자 여러 개
-.*?	    가능한 최소 문자 매칭
+# r''	    Raw 문자열 (\를 그대로 사용)
 
-예) (1111) ssdfdfd (2222) sdfdfdfasdf (33333)
+# 기호	설명
+# ( )	    그룹(캡처 그룹)
+# .	    아무 문자 1개
+# *	    0개 이상 반복
+# +       1개 이상 반복
+# ?	    최소 매칭(Non-Greedy)
+# .*	    모든 문자 여러 개
+# .*?	    가능한 최소 문자 매칭
 
-\d	    숫자 1개 (0~9)
-[0-9]   숫자 1개 (0~9)
-{4}	    정확히 4번 반복
-{2}	    정확히 2번 반복
-Year	문자열 "Year" 그대로
-Month	문자열 "Month" 그대로
-Day	    문자열 "Day" 그대로
-'''
+# 예) (1111) ssdfdfd (2222) sdfdfdfasdf (33333)
+
+# \d	    숫자 1개 (0~9)
+# [0-9]   숫자 1개 (0~9)
+# {4}	    정확히 4번 반복
+# {2}	    정확히 2번 반복
+# Year	문자열 "Year" 그대로
+# Month	문자열 "Month" 그대로
+# Day	    문자열 "Day" 그대로
+
 
 
 def rename_dates(root: Path):
@@ -40,6 +40,8 @@ def rename_dates(root: Path):
             if not match:
                 continue
 
+            # (a, b, c) = (1, 2, 3) -> a=1, b=2, c=3
+            # a, b, c = (1, 2, 3) -> a=1, b=2, c=3
             pre, yyyy, mm, dd, suf = match.groups()
 
             new_name = f"{pre}_{yyyy}-{mm}-{dd}{suf}"
@@ -54,7 +56,9 @@ def rename_dates(root: Path):
 
             shutil.move(str(src), str(dst))
             print(f"[리네임] {name} -> {dst.name}")
+            
 
 if __name__ == "__main__":
     base = Path(__file__).parent / "연습" / "날짜정리"
+    base.mkdir(parents=True, exist_ok=True)
     rename_dates(base)
