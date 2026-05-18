369d4cfd73447ae07d776c9a07bde9c808607208
fsclass-n <whyeil@naver.com>
2026-05-18 17:31:18 +0900

    하하

--- rpa/p03_데스크탑/ch04_마우스자동화/ex04_이미지인식.py
+++ rpa/p03_데스크탑/ch04_마우스자동화/ex04_이미지인식.py
@@ -1,7 +1,12 @@
 import pyautogui
+from pathlib import Path
+
+cur_path = Path(__file__).parent
 
 try:
-    box = pyautogui.locateOnScreen("setting.png")
+    img_path = str(cur_path / "setting.png")
+    print("img_path:", img_path)
+    box = pyautogui.locateOnScreen(img_path)
     print("box:", box)
     # box: Box(left=187, top=841, width=34, height=39)
     pyautogui.moveTo(box.left + box.width // 2, box.top + box.height // 2, duration=0.2)
@@ -12,14 +17,14 @@ except Exception as e:
     print("알수 없는 에러:", e)
 
 
-try:
-    items = list(pyautogui.locateAllOnScreen("folder.png"))
-    print("items:", items)
+# try:
+#     items = list(pyautogui.locateAllOnScreen("folder.png"))
+#     print("items:", items)
 
-    for i, b in enumerate(items):
-        print(f"{i+1}번째 박스 : ", b)
+#     for i, b in enumerate(items):
+#         print(f"{i+1}번째 박스 : ", b)
 
-except pyautogui.ImageNotFoundException:
-    print("해당하는 이미지를 찾을 수 없습니다.")
-except Exception as e:
-    print("알수 없는 에러:", e)
+# except pyautogui.ImageNotFoundException:
+#     print("해당하는 이미지를 찾을 수 없습니다.")
+# except Exception as e:
+#     print("알수 없는 에러:", e)
